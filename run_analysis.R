7a9f376eddbeb8ad9d3188e7b907be3f43a276c2
caresiri <carlos.r.siri@gmail.com>
2016-02-27 23:37:25 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,14 +2,14 @@
 library(dplyr)
 setwd("./Desktop")
 if(!file.exists("./data")){dir.create("./data")}
-#download two data sets and read them both into two datadframes
+
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
 setwd("./data")
 getwd()
 unzip("Dataset.zip")
 
-# Merges the training and the test sets to create one data set.
+
 xtrain <- read.table('./UCI HAR Dataset/train/X_train.txt')
 xtest <- read.table('./UCI HAR Dataset/test/X_test.txt')
 x <- rbind(xtrain, xtest)
@@ -22,12 +22,10 @@ ytrain <- read.table('./UCI HAR Dataset/train/y_train.txt')
 ytest <- read.table('./UCI HAR Dataset/test/y_test.txt')
 y <- rbind(ytrain, ytest)
 
-# Extracts only the measurements on the mean and standard deviation for each measurement. 
 features <- read.table('./UCI HAR Dataset/features.txt')
 mean.sd <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
 xmean.sd <- x[, mean.sd]
 
-# Uses descriptive activity names to name the activities in the data set
 names(xmean.sd) <- features[mean.sd, 2]
 names(xmean.sd) <- tolower(names(xmean.sd)) 
 names(xmean.sd) <- gsub("\\(|\\)", "", names(xmean.sd))
@@ -40,12 +38,10 @@ y[, 1] = activities[y[, 1], 2]
 colnames(y) <- 'activity'
 colnames(subj) <- 'subject'
 
-# Appropriately labels the data set with descriptive activity names.
 data <- cbind(subj, xmean.sd, y)
 str(data)
 write.table(data, './merged.txt', row.names = F)
 
-# Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 average.df <- aggregate(x=data, by=list(activities=data$activity, subj=data$subject), FUN=mean)
 average.df <- average.df[, !(colnames(average.df) %in% c("subj", "activity"))]
 str(average.df)
